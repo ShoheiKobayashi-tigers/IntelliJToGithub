0fb93b25c4cf3869e7c0002df8367bbbdacc378f
ShoheiKobayashi <koba.5387.tigers@gmail.com>
2025-04-14 17:02:45 +0900

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,19 @@
 package org.example;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello, World!");
-  }
+
+    LocalDateTime time=LocalDateTime.of(2025,7,25,13,0);
+    LocalDateTime lastTime=LocalDateTime.of(2025,7,25,15,0);
+    List<LocalDateTime> times=new ArrayList<>();
+    times.add(time);
+    times.add(lastTime);
+    System.out.println(times);
+    }
 }
